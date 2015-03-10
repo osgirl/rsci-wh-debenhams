a1e3803daa7ae7f23f065ed57477bee8f607f8be
Dean <fcasili@stratpoint.com>
2015-03-10 15:59:06 +0800

    updates

--- app/cron/ewms_cron_dump.php
+++ app/cron/ewms_cron_dump.php
@@ -7,7 +7,7 @@ include_once('ewms_cron_class.php');
 
 $ewms = new cronEWMS();
 
-/*$ewms->products();
+$ewms->products();
 sleep(10);
 $ewms->department();
 sleep(10);
@@ -27,7 +27,7 @@ $ewms->storeOrderDetails();
 sleep(10);
 $ewms->storeReturn();
 sleep(10);
-$ewms->storeReturnDetail();*/
+$ewms->storeReturnDetail();
 $ewms->picklist();
 sleep(10);
 $ewms->picklistDetail();
