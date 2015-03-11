a2c8b8ab2d0eee75ec87df6b91ee8545c03ef1f3
dean <fcasili@stratpoint.com>
2015-03-11 16:18:01 +0800

    updates

--- app/cron/cron_10am.php
+++ app/cron/cron_10am.php
@@ -5,8 +5,8 @@ include_once('db2_cron_class.php');
 
 $db2 = new cronDB2();
 
-/*$db2->purchaseOrder();			sleep(10);
-$db2->purchaseOrderDetails();	sleep(10);*/
+$db2->purchaseOrder();			sleep(10);
+$db2->purchaseOrderDetails();	sleep(10);
 $db2->storeReturn(); sleep(10);
 $db2->storeReturnDetails(); sleep(10);
 // $db2->inventory();				sleep(10);
