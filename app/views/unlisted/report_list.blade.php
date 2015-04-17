62be3b397f0cb7810c1da52329e363b0a3165eec
taleen <kristaleen.delacruz@gmail.com>
2015-04-17 08:37:21 +0800

    returned html script

--- app/views/unlisted/report_list.blade.php
+++ app/views/unlisted/report_list.blade.php
@@ -22,6 +22,9 @@
 <title></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta name="apple-mobile-web-app-capable" content="yes">
+{{ HTML::style('resources/css/bootstrap.min.css') }}
+{{ HTML::style('resources/css/bootstrap-responsive.min.css') }}
+{{ HTML::style('resources/css/style.css') }}
 </head>
 <body>
 <div class="table-responsive">
