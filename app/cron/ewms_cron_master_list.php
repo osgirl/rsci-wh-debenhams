b295b547995e8dc73e3261acb4143630421cd866
Dean <fcasili@stratpoint.com>
2015-04-20 10:43:04 +0800

    update on master list cron

--- app/cron/ewms_cron_master_list.php
+++ app/cron/ewms_cron_master_list.php
@@ -6,7 +6,7 @@ include_once('ewms_cron_class.php');
 $ewms = new cronEWMS();
 
 $ewms->products();		sleep(10);
-$ewms->slots();			sleep(10);
+$ewms->department();			sleep(10);
 $ewms->slots();			sleep(10);
 $ewms->vendors();		sleep(10);
 $ewms->stores();		sleep(10);
