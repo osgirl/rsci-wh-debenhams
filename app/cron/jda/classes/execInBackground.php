3eb40e96442c5cbfd0eec525aa668f18d50e8068
taleen <kristaleen.delacruz@gmail.com>
2015-04-28 10:41:12 +0800

    class for logging execution

---
+++ app/cron/jda/classes/execInBackground.php
@@ -0,0 +1,12 @@
+<?php
+
+    	if($argv[1]) $filename = ((isset($argv[1]))? $argv[1] : NULL);
+    	if($argv[2]) $data = ((isset($argv[2]))? $argv[2] : NULL);
+        $cmd = 'nohup php -q ' . __DIR__.'/'.$filename.'.php '.$data;
+    	$pidfile = __DIR__.'/../../jda/logs/pidfile.log';
+
+    	$source=$filename . "_" . date('m_d_y');
+    	$outputfile = __DIR__.'/../../jda/logs/'.$source.'.log';
+        exec(sprintf("%s >> %s 2>&1 & echo $! >> %s", $cmd, $outputfile, $pidfile));
+        // exec($cmd . " </dev/null 2> /dev/null & echo $!");
+        // exec($cmd . " > /dev/null &");
